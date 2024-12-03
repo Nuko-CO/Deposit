03720b0f79248289bc26c3399702dd23b317848c
Nuko-CO <75743680+Nuko-CO@users.noreply.github.com>
2024-12-03 17:13:40 +0500

    Initial Commit

---
+++ Deposit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Deposit
+//
+//  Created by Nurdaulet Darmenov on 12/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Deposit/DepositApp.swift
@@ -0,0 +1,17 @@
+//
+//  DepositApp.swift
+//  Deposit
+//
+//  Created by Nurdaulet Darmenov on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DepositApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
